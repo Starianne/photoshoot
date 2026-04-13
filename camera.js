543bce7dadd5205b4172aa91568f5f5be0c06512
[Annelies S] <118388665+Starianne@users.noreply.github.com>
2026-04-13 04:12:36 +0100

    photobooth works

---
+++ camera.js
@@ -0,0 +1,38 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const photo = document.getElementById('photo');
+const cameraBtn = document.getElementById('cameraBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+
+downloadBtn.disabled = true;
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(err => {
+    console.error("Camera access denied:", err);
+  });
+
+cameraBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        const context = canvas.getContext('2d');
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        context.drawImage(video, 0, 0);
+
+        const dataURL = canvas.toDataURL('image/png');
+        photo.src = dataURL;
+        downloadBtn.disabled = false;
+    }, 3000);
+
+});        
+
+downloadBtn.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL();
+    link.download = 'photo.png';
+    link.click();
+        });
